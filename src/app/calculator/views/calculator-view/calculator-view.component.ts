1b0431082fd652c81be389714683ecf1d85c6b97
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-11-24 23:02:40 -0300

    feature: Component separation

--- src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorComponent } from '../../components/calculator/calculator.component';
 
 @Component({
   selector: 'calculator-view',
-  imports: [],
+  imports: [
+    CalculatorComponent
+  ],
   templateUrl: './calculator-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
