789cdbb5ff83fa67224b29a177bb879bcda2bae4
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-01-04 20:54:07 -0300

    test: Build number and calculate

--- src/app/calculator/services/calculator.service.spec.ts
+++ src/app/calculator/services/calculator.service.spec.ts
@@ -2,21 +2,102 @@ import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 
 describe('CalculatorService', () => {       
-let service: CalculatorService;
+    let service: CalculatorService;
 
-beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
-});
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CalculatorService);
+    });
 
-it('should be created', () => {
-    expect(service).toBeTruthy();
-});
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-it('should be created with default values', () => {
-    expect(service.resultText()).toBe('0');
-    expect(service.subResultText()).toBe('0');
-    expect(service.lastOperator()).toBe('+');
-});
+    it('should be created with default values', () => {
+        expect(service.resultText()).toBe('0');
+        expect(service.subResultText()).toBe('0');
+        expect(service.lastOperator()).toBe('+');
+    });
+
+    it('should set resultText, subResultText to "0" when C is pressed', () => {
+        service.resultText.set('1');
+        service.subResultText.set('1');
+        service.lastOperator.set('-');
+        service.constructNumber('C');	
+        expect(service.resultText()).toBe('0');
+        expect(service.subResultText()).toBe('0');
+        expect(service.lastOperator()).toBe('+');
+    });
+
+    it('should update resulText with number input', () => {
+        service.constructNumber('1');
+        expect(service.resultText()).toBe('1');
+
+        service.constructNumber('2');
+        expect(service.resultText()).toBe('12');
+    });
+
+    it('should handle operators correctly', () => {
+        service.constructNumber('1');
+        service.constructNumber('+');
+
+        expect(service.lastOperator()).toBe('+');
+        expect(service.subResultText()).toBe('1');
+        expect(service.resultText()).toBe('0');
+    });
+
+    it('should calculate result correctly for addition', () => {
+        service.constructNumber('1');
+        service.constructNumber('+');
+        service.constructNumber('2');
+        service.constructNumber('=');
+
+        expect(service.resultText()).toBe('3');    
+    });
+
+    it('should calculate result correctly for subtraction', () => {
+        service.constructNumber('4');
+        service.constructNumber('-');
+        service.constructNumber('2');
+        service.constructNumber('=');
+
+        expect(service.resultText()).toBe('2');    
+    });
+
+    it('should calculate result correctly for multiplication', () => {
+        service.constructNumber('2');
+        service.constructNumber('x');
+        service.constructNumber('2');
+        service.constructNumber('=');
+
+        expect(service.resultText()).toBe('4');    
+    });
+
+    it('should calculate result correctly for division', () => {
+        service.constructNumber('1');
+        service.constructNumber('2');
+        service.constructNumber('÷');
+        service.constructNumber('2');
+        service.constructNumber('=');
+
+        expect(service.resultText()).toBe('6');    
+    });
+
+    it('should handle decimal point correctly start', () => {
+        service.constructNumber('1');
+        service.constructNumber('.');
+        service.constructNumber('5');
+
+        expect(service.resultText()).toBe('1.5');    
+    });
+
+    it('should handle decimal point correctly starting with zero', () => {
+        service.constructNumber('0');
+        service.constructNumber('.');
+        service.constructNumber('.');
+        service.constructNumber('5');
+
+        expect(service.resultText()).toBe('0.5');    
+    });
 
 });
