f1675cbdcafce9da8eedc1de3eee8684abadc0f6
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-01-07 23:25:10 -0300

    test: Button click

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -0,0 +1,60 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CalculatorButtonComponent } from './calculator-button.component';
+
+describe('CalculatorButtonComponent', () => {
+
+  let fixture: ComponentFixture<CalculatorButtonComponent>;
+  let compiled: HTMLElement;
+  let component: CalculatorButtonComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+    imports: [CalculatorButtonComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorButtonComponent);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+});
+
+  it('should create CalculatorButtonComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should apply w-1/4 doubleSize is false', () => {    
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+    expect(hostCssClasses).toContain('w-1/4');
+    expect(component.isDoubleSize()).toBeFalse();
+  });
+
+  it('should apply w-2/4 doubleSize is true', () => {    
+    fixture.componentRef.setInput('isDoubleSize', true);
+    fixture.detectChanges();
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+    expect(hostCssClasses).toContain('w-2/4');
+    expect(component.isDoubleSize()).toBeTrue();
+  });
+
+  it('should emit onClick when handleClick is called', () => {
+    spyOn(component.onClick, 'emit');
+    component.handleClick();
+    expect(component.onClick.emit).toHaveBeenCalled();
+  });
+
+  it('should set isPressed to true and the false when keyboardPressStyle is called whith matching key', (done) => {
+    component.contentValue()!.nativeElement.innerText = '1';
+    component.keyboardPressedStyle('1');
+
+    expect(component.isPressed()).toBeTrue();
+    setTimeout(() => {
+      expect(component.isPressed()).toBeFalse();
+      done();
+    }, 101);
+  });
+
+  /* it('should contain ng-content', () => { 
+    expect(compiled.querySelector('ng-content')).not.toBeNull();
+  }); */
+
+});
